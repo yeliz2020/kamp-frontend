21474e8297d8e5f1bcf27325eb232785805ab19c
Yeliz <yeliz.isler@gmail.com>
2021-03-10 22:48:40 +0300

    frontend

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import {HttpClient} from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  apiUrl="https://localhost:44339/api/products/getall"
+  //productResponseModel:ProductResponseModel={};
+  constructor(private httpClient:HttpClient) {}
+
+  ngOnInit(): void {
+    console.log('Init çalıştı');
+  }
+
+  getProducts() {
+    this.httpClient.get<ProductResponseModel>(this.apiUrl)
+  }
+}
