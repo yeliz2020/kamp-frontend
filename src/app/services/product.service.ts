ad6d00617b6966a7b906c927c0026c6ac3b6cb57
Yeliz <yeliz.isler@gmail.com>
2021-03-13 23:59:29 +0300

    13 mart ders sonu

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,18 +1,25 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/ListResponseModel';
 import { Product } from '../models/product';
 
-
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ProductService {
-  apiUrl = 'https://localhost:44339/api/products/getall';
-  constructor(private httpClient: HttpClient) {}
-  //apiden veri almak için
-  getProducts(): Observable<ListResponseModel<Product>> {
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+
+  apiUrl = 'https://localhost:44314/api/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts():Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "products/getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 }
